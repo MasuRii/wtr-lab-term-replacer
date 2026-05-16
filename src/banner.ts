54701ebb9ccae295cd3e0e4f69315d68d69f53de
MasuRii <masurii@users.noreply.github.com>
2026-05-16 18:55:30 +0800

    chore(release): bump version to 5.7.0

--- src/banner.ts
+++ src/banner.ts
@@ -1,10 +1,10 @@
 /**
- * WTR Lab Term Replacer v5.6.0
- * Built: 2026-04-30 (production)
+ * WTR Lab Term Replacer v5.7.0
+ * Built: 2026-05-16 (production)
  *
  * A modular, Webpack-powered TypeScript version of the WTR Lab Term Replacer userscript.
  *
- * @version 5.6.0
+ * @version 5.7.0
  * @build production
- * @date 2026-04-30
+ * @date 2026-05-16
  */
